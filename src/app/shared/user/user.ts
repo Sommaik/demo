9cb3b5587593820251b820264e29589efbffebe0
sommaik <sommai.k@gmail.com>
2017-08-30 10:49:02 +0700

    support add / update function

---
+++ src/app/shared/user/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    email :string;
+    firstName:string;
+    lastName:string;
+    mobile:string;
+    password:string;
+    rePassword:string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user/user';
+import { UserService } from '../shared/user/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserService]
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  mode: string = "ADD";
+  id: number = 0;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+  ) {
+    this.user = new User();
+  }
 
   ngOnInit() {
+    this.activeRoute.params.subscribe(params => {
+      if (params['id']) {
+        let id = params['id'];
+        this.userService.findById(id).subscribe(
+          user => {
+            this.user = user;
+          }, error => {
+            console.log(error);
+          });
+        this.mode = "EDIT";
+        this.id = id;
+      }
+    });
+  }
+
+  onSave() {
+    if (this.mode === "EDIT") {
+      this.userService.updateItem(this.id, this.user).subscribe(
+        data => {
+          Materialize.toast('Update item complete', 1000);
+          this.router.navigate(['support', 'user-list']);
+        },
+        err => {
+          console.log(err);
+        }
+      )
+    } else {
+      this.userService.addItem(this.user).subscribe(
+        datas => {
+          Materialize.toast('Add new item complete', 1000);
+          this.router.navigate(['support', 'user-list']);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
   }
 
 }
