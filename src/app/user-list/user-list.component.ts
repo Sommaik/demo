21442f5a805cab5193cc25e53fca03865a1bd8d1
sommaik <sommai.k@gmail.com>
2017-08-30 11:39:16 +0700

    use list image

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,22 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../shared/user/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  providers: [UserService]
 })
 export class UserListComponent implements OnInit {
-
-  constructor(private router:Router) { 
+  userData = [];
+  imgUrl = "http://localhost:3000/user/profile/"
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {
 
   }
 
   ngOnInit() {
+    this.loadData();
   }
 
-  onAddButtonClick(){
+  onAddButtonClick() {
     this.router.navigate(['support', 'user']);
+  }
+
+  onDeleteClick(id){
+    this.userService.deleteItem(id).subscribe((datas)=>{
+      this.loadData();
+    });
+  }
+
+  loadData(){
+    this.userService.loadItem().subscribe((datas)=>{
+      this.userData = datas;
+    });
   }
 
 }
